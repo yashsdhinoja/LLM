70b09172579a0727e8f6c1103002c0d250949fa7
yashsdhinoja <yash.s.dhinoja7007@gmail.com>
2026-09-22 11:30:39 +0530

    Numpy Methods Exercise

---
+++ Numpy/1.Methods/Exercise/1.array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+array = np.array([5, 10, 15, 20])
+print(array)
+
+zeros = np.zeros([3,4])
+print(zeros)
+
+ones = np.ones(6)
+print(ones)
+
+full = np.full((2,2), 99)
+print(full)
+
+arange = np.arange(10, 20, 2)
+print(arange)
+
+identity_matrices = np.eye(5)
+print(identity_matrices)
